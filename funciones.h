9460d298dfd48cca173fe282e6e91b822c1f4b5d
Alejandro Ramirez <alejandro.ramirez.v@ucb.edu.bo>
2024-06-04 12:58:42 -0400

    Agregando la funcion de verificacion de datos de entrada

--- funciones.h
+++ funciones.h
@@ -7,3 +7,11 @@
 #include <string>
 using namespace std;
 
+struct input{ //creo las estructuras de los datos que vamos a utilizar
+    int year;
+    int month;
+    int day;
+    string event;
+};
+
+void check_data (vector<input> &data_hub);
--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include "funciones.h"
 
+//g++ main.cpp funciones.cpp -o database
+
 int main() {
+    vector<input> data_hub; //creo un vector de estructuras que guardara los datos
+    check_data (data_hub);
     return 0;
 }
