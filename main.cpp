a79f57d8ab15886adaca392628d584607e211ba1
Alejandro Ramirez <alejandro.ramirez.v@ucb.edu.bo>
2024-06-04 19:51:21 -0400

    Agregando los comandos

--- main.cpp
+++ main.cpp
@@ -5,6 +5,30 @@
 int main() {
     vector<input> data_hub; //creo un vector de estructuras que guardara los datos
     database_in(data_hub);
-    check_data (data_hub);
+
+    while (true) {
+        string command;
+        cin>>command;
+        cin.ignore();
+        if (command == "Add" || command == "ADD" || command == "add") {
+            check_data (data_hub); //como le entrada del comando esta en formato fecha-evento se usa check_data
+            delete_identical_events (data_hub); //se elimina el evento si es repetido
+            ascending_order (data_hub); //y luego se reoordenada de forma ascentende todo el vector data_hub
+        }
+        else if (command == "Del" || command == "DEL" || command == "del") {
+            //codigo
+        }
+        else if (command == "Find" || command == "FIND" || command == "find") {
+            //codigo
+        }
+        else if (command == "Print" || command == "PRINT" || command == "print") {
+            //codigo
+        }
+        else {
+            cout<<"Unknown command: "<<command<<endl;
+        }
+    }
+
     return 0;
 }
+
